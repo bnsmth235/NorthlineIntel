fc06acbd52e98d799f2322f47e312900ea5bd762
Ben Smith <a02300150@usu.edu>
2023-02-26 10:26:39 -0700

    Bare Bones

--- MoffatIntel/reports/urls.py
+++ MoffatIntel/reports/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = "reports"
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views)
+    path('home/', views.home, name='home'),
+    path('log_out/', views.log_out, name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
