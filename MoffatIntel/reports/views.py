2c1604fa3b1f296d972c9e00b233c5cc0478585b
Ben Smith <a02300150@usu.edu>
2023-05-17 14:16:48 -0600

    Fixed refresh issue

--- MoffatIntel/reports/views.py
+++ MoffatIntel/reports/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth import authenticate, logout, login
+from django.views.decorators.csrf import ensure_csrf_cookie
+
 from .models import *
 
 
+@ensure_csrf_cookie
 def index(request):
     username = ""
     password = ""
@@ -15,12 +18,22 @@ def index(request):
 
         if user is not None:
             login(request, user)
-            return render(request, 'reports/home.html', {'user': user})
-    else:
-        if(username == "" and password == ""):
-            return render(request, 'reports/login.html', {'error_message': "Please input login credentials"})
+            refresh = request.POST.get('refresh', None)
+            if refresh:
+                return redirect('reports:home')
+            else:
+                redirect_url = request.POST.get('next', None)
+                if redirect_url:
+                    return redirect(resolve_url(redirect_url))
+                else:
+                    return redirect('reports:home')
         else:
-            return render(request, 'reports/login.html', {'error_message': "The provided credentials are incorrect"})
+            if(username == "" and password == ""):
+                return render(request, 'reports/login.html', {'error_message': "Please input login credentials"})
+            else:
+                return render(request, 'reports/login.html', {'error_message': "The provided credentials are incorrect"})
+    else:
+        return render(request, 'reports/login.html')
 
 
 def log_out(request):
@@ -31,7 +44,7 @@ def log_out(request):
 def home(request):
     if not request.user.is_authenticated:
            return redirect('%s?next=%s' % ('login/', request.path))
-    recent_reports = Project.objects.order_by('-last_edit_date', '-status')[:5]
-    context = {'recent_reports': recent_reports}
+    recent_projects = Project.objects.order_by('-last_edit_date', '-status')[:5]
+    context = {'recent_projects': recent_projects}
     return render(request, 'reports/home.html', context)
 
