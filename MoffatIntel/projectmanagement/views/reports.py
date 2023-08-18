0575daf4b7d9122cadbe035bc5d59f9aa5874d2f
Ben Smith <bnsmth235@gmail.com>
2023-08-18 13:36:09 -0600

    Fixed registration and reports

--- MoffatIntel/projectmanagement/views/reports.py
+++ MoffatIntel/projectmanagement/views/reports.py
@@ -6,4 +6,4 @@ from ..models import Project, Draw, Group, Subgroup, Subcontractor, Vendor, Repo
 @login_required(login_url='projectmanagement:login')
 def reports(request):
     reports = Report.objects.order_by('-date')
-    return render(request, 'projectmanagement/all_reports.html', {'projectmanagement': reports})
+    return render(request, 'reports/all_reports.html', {'reports': reports})
